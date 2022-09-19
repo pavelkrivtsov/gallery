86c9aab377db89d21ae8d1990c6ac935fd378a7e
Павел Кривцов <pavel.krivcov@yandex.ru>
2022-09-19 19:48:29 +0300

    Initial commit

---
+++ gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gallery
+//
+//  Created by Павел Кривцов on 19.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
